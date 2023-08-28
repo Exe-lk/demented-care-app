7daa39b215bc99039d56df8129a1b17733444825
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-08-28 20:45:36 +0530

    Added Doctor details Activity
    Created Doctordetails Activity and list view layout and fetch the data from firebase db

---
+++ app-android/app/src/main/java/com/example/dementedcare/Doctordetails.java
@@ -0,0 +1,105 @@
+package com.example.dementedcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class Doctordetails extends AppCompatActivity {
+
+    RecyclerView recyclerView;  // RecyclerView
+    ArrayList<data> userArrayList;   // Data java class
+    MyAdapter myAdapter;    // MyAdapter class
+    FirebaseFirestore db;    // Database
+    ProgressDialog progressDialog; //progress bar
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctordetails);
+
+//       start code
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data....");
+        progressDialog.show();
+
+        recyclerView = findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize Firebase Firestore database
+        db = FirebaseFirestore.getInstance();
+        userArrayList = new ArrayList<>();
+        myAdapter = new MyAdapter(this, userArrayList);
+
+        myAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(data doctor) {
+                openDoctorDetailsActivity(doctor);
+            }
+        });
+
+        recyclerView.setAdapter(myAdapter);
+
+        // Getting the data from Firestore
+        EventChangeListener();
+    }
+
+    private void openDoctorDetailsActivity(data doctor) {
+        Log.d("listView", "Opening DoctorDetailsActivity");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("selected_doctor", doctor);
+        startActivity(intent);
+    }
+
+    private void EventChangeListener() {
+        // Referring to the collection
+        db.collection("doctors").orderBy("email", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        // Callback method
+                        if (error != null) {
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                            Log.e("Firebase error", error.getMessage());
+                            return;
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                userArrayList.add(dc.getDocument().toObject(data.class));
+                            }
+                        }
+                        myAdapter.notifyDataSetChanged();
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                    }
+                });
+    }
+    //        end code
+}
+
+
+
+
+
--- app-android/app/src/main/java/com/example/dementedcare/data.java
+++ app-android/app/src/main/java/com/example/dementedcare/data.java
@@ -1,4 +1,48 @@
 package com.example.dementedcare;
 
-public class data {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class data implements Parcelable {
+    String email;
+
+
+    public data() {
+        // Default constructor
+    }
+
+    protected data(Parcel in) {
+        email = in.readString();
+    }
+
+    public static final Creator<data> CREATOR = new Creator<data>() {
+        @Override
+        public data createFromParcel(Parcel in) {
+            return new data(in);
+        }
+
+        @Override
+        public data[] newArray(int size) {
+            return new data[size];
+        }
+    };
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Implement the Parcelable methods
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(email);
+    }
 }
