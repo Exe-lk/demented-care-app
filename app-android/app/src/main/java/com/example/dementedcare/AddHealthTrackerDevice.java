2838107b8b24f11c872812ae28af26f3723512ea
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-08-30 12:31:39 +0530

    Added new Gradle dependencies for QR Scanner
    add new 2 dependencies build.gradle(:app)

---
+++ app-android/app/src/main/java/com/example/dementedcare/AddHealthTrackerDevice.java
@@ -0,0 +1,14 @@
+package com.example.dementedcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHealthTrackerDevice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_health_tracker_device);
+    }
+}
