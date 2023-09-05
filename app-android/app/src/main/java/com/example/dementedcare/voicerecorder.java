e3bcba586257ed3b329c17c34c005b3887f982f8
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-09-05 09:23:14 +0530

    Created Voice recorder activity.
    Voice recorder activity strat recorder butoon created

--- app-android/app/src/main/java/com/example/dementedcare/voicerecorder.java
+++ app-android/app/src/main/java/com/example/dementedcare/voicerecorder.java
@@ -1,14 +1,103 @@
 package com.example.dementedcare;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import java.io.IOException;
 
 public class voicerecorder extends AppCompatActivity {
+
+    private Button recordButton, playButton;
+    private MediaRecorder mediaRecorder;
+    private String audioFilePath;
+    private boolean isRecording = false;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voicerecorder);
+
+        recordButton = findViewById(R.id.recordButton);
+        playButton = findViewById(R.id.playButton);
+
+        audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.3gp";
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRecording) {
+                    startRecording();
+                    recordButton.setText("Stop Recording");
+                    playButton.setEnabled(false);
+                } else {
+                    stopRecording();
+                    recordButton.setText("Record");
+                    playButton.setEnabled(true);
+                }
+                isRecording = !isRecording;
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playRecording();
+            }
+        });
+    }
+
+    private void startRecording() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(audioFilePath);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecording() {
+        if (mediaRecorder != null) {
+            try {
+                mediaRecorder.stop();
+                mediaRecorder.release();
+                mediaRecorder = null;
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+        // Rest of your code...
+    }
+
+    private void playRecording() {
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(audioFilePath);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaRecorder != null) {
+            mediaRecorder.release();
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
     }
 }
