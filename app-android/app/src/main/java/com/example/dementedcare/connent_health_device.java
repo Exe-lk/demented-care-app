da31f88bc3f09282c8be25280312994a1f62732c
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-09-08 20:59:28 +0530

    Added pair helth device activity
    I created pair health device activity.

---
+++ app-android/app/src/main/java/com/example/dementedcare/connent_health_device.java
@@ -0,0 +1,14 @@
+package com.example.dementedcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class connent_health_device extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connent_health_device);
+    }
+}
--- app-android/app/src/main/java/com/example/dementedcare/pair_health_device.java
+++ app-android/app/src/main/java/com/example/dementedcare/pair_health_device.java
@@ -2,13 +2,57 @@ package com.example.dementedcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+
 
 public class pair_health_device extends AppCompatActivity {
+
+    private EditText name;
+    private Button add;
+
+    private TextView deviceIdTextView;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pair_health_device);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Initialize your TextView from your XML layout
+        deviceIdTextView = findViewById(R.id.deviceIdTextView);
+
+        // Query the "device_id" field from the database
+        databaseReference.child("health_checking_device").child("HLD001").child("device_id")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String deviceId = dataSnapshot.getValue(String.class);
+                        if (deviceId != null) {
+                            deviceIdTextView.setText(deviceId);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle any errors here
+                    }
+                });
     }
 }
