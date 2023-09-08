98d977730c5c3c0f6f75bea9413a27578f6cda25
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-09-09 00:18:02 +0530

    Add all data in firestore
    I created PatientHeakthDetails.java activity for new button and that button click after saved all data in firebase firestore.

--- app-android/app/src/main/java/com/example/dementedcare/PatientHealthDetails.java
+++ app-android/app/src/main/java/com/example/dementedcare/PatientHealthDetails.java
@@ -2,21 +2,29 @@ package com.example.dementedcare;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.annotation.NonNull;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import android.widget.Button;
-import android.view.View;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FieldValue;
 
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class PatientHealthDetails extends AppCompatActivity {
 
@@ -26,11 +34,20 @@ public class PatientHealthDetails extends AppCompatActivity {
     private TextView heartRateTextView;
     private TextView bloodO2TextView;
 
+    private EditText nicNumberEditText;
+    private Button addDataButton;
+    private FirebaseFirestore firestore;
+    private Double bodyTemperatureDouble;
+    private Double bloodPressureDouble;
+    private Double heartRateDouble;
+    private Double bloodO2Double;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_health_details);
 
+        firestore = FirebaseFirestore.getInstance();
         // Initialize Firebase Database
         mDatabase = FirebaseDatabase.getInstance().getReference().child("health_device");
 
@@ -40,6 +57,9 @@ public class PatientHealthDetails extends AppCompatActivity {
         heartRateTextView = findViewById(R.id.heartrate);
         bloodO2TextView = findViewById(R.id.bloodox2);
         Button aiPredictionButton = findViewById(R.id.aibtn);
+
+        nicNumberEditText = findViewById(R.id.nicNumberEditText);
+        addDataButton = findViewById(R.id.addDataButton);
 
         // Add ValueEventListener to listen for data changes
 
@@ -56,10 +76,10 @@ public class PatientHealthDetails extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // Retrieve data for each health parameter
-                Double bodyTemperatureDouble = dataSnapshot.child("body_temperature").getValue(Double.class);
-                Double bloodPressureDouble = dataSnapshot.child("blood_pressure").getValue(Double.class);
-                Double heartRateDouble = dataSnapshot.child("heart_rate").getValue(Double.class);
-                Double bloodO2Double = dataSnapshot.child("blood_oxygen_level").getValue(Double.class);
+                bodyTemperatureDouble = dataSnapshot.child("body_temperature").getValue(Double.class);
+                bloodPressureDouble = dataSnapshot.child("blood_pressure").getValue(Double.class);
+                heartRateDouble = dataSnapshot.child("heart_rate").getValue(Double.class);
+                bloodO2Double = dataSnapshot.child("blood_oxygen_level").getValue(Double.class);
 
                 // Convert Double values to String
                 String bodyTemperature = String.valueOf(bodyTemperatureDouble);
@@ -79,5 +99,53 @@ public class PatientHealthDetails extends AppCompatActivity {
                 // Handle errors, if any
             }
         });
+
+        addDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the NIC number entered by the user
+                String patientId = nicNumberEditText.getText().toString().trim(); // Assuming patientId is the same as NIC number
+
+                // Check if patientId is not empty
+                if (!patientId.isEmpty()) {
+                    // Create a map to store the data
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("bodyTemperature", bodyTemperatureDouble);
+                    data.put("bloodPressure", bloodPressureDouble);
+                    data.put("heartRate", heartRateDouble);
+                    data.put("bloodO2", bloodO2Double);
+                    data.put("timestamp", FieldValue.serverTimestamp()); // Real-time timestamp
+                    data.put("patientId", patientId);
+
+                    // Store data in Firestore
+                    firestore.collection("patient_past_data") // Collection name is "patient_past_data"
+                            .document(patientId) // Document name is the patientId
+                            .set(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // Data added successfully
+                                    Toast.makeText(PatientHealthDetails.this, "Data added to Firestore", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Error handling
+                                    Toast.makeText(PatientHealthDetails.this, "Failed to add data to Firestore", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    // NIC number is empty, show an error message
+                    Toast.makeText(PatientHealthDetails.this, "Please enter patientId", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Helper method to get the current timestamp as a string
+    private String getCurrentTimestamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+        return sdf.format(new Date());
     }
 }
--- app-android/app/src/main/java/com/example/dementedcare/pair_health_device.java
+++ app-android/app/src/main/java/com/example/dementedcare/pair_health_device.java
@@ -1,30 +1,25 @@
 package com.example.dementedcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class pair_health_device extends AppCompatActivity {
 
     private EditText name;
     private Button add;
-
     private TextView deviceIdTextView;
     private DatabaseReference databaseReference;
 
@@ -37,6 +32,19 @@ public class pair_health_device extends AppCompatActivity {
 
         // Initialize your TextView from your XML layout
         deviceIdTextView = findViewById(R.id.deviceIdTextView);
+
+        // Initialize the QR code scanner
+        final IntentIntegrator qrScanner = new IntentIntegrator(this);
+        qrScanner.setOrientationLocked(false);
+
+        // Start scanning when the "Scan QR Code" button is clicked
+        add = findViewById(R.id.scanQRButton);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qrScanner.initiateScan();
+            }
+        });
 
         // Query the "device_id" field from the database
         databaseReference.child("health_checking_device").child("HLD001").child("device_id")
@@ -54,5 +62,26 @@ public class pair_health_device extends AppCompatActivity {
                         // Handle any errors here
                     }
                 });
+    }
+
+    // Override onActivityResult to handle the result of the QR code scan
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String scannedValue = result.getContents();
+            String deviceId = deviceIdTextView.getText().toString();
+
+            if (scannedValue != null && scannedValue.equals(deviceId)) {
+                // QR code value matches device_id, open a new activity
+                Intent intent = new Intent(this, PatientHealthDetails.class); // Replace YourNewActivity with the actual activity class you want to open
+                startActivity(intent);
+            } else {
+                // Handle the case where the QR code value doesn't match
+                // You can show a message to the user or take appropriate action here
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
